81207fda57cee3852f6476dc7c8c0e7b19124c4b
vb <>
2020-09-14 17:06:44 +0700

    - Config Saga

--- src/app/store/index.js
+++ src/app/store/index.js
@@ -2,7 +2,9 @@ import * as reduxModule from "redux";
 import { applyMiddleware, compose, createStore } from "redux";
 import createReducer from "./reducers";
 import thunk from "redux-thunk";
-
+import createSagaMiddleware from 'redux-saga';
+import rootSaga from './rootSaga'
+const saga = createSagaMiddleware();
 /*
 Fix for Firefox redux dev tools extension
 https://github.com/zalmoxisus/redux-devtools-instrument/pull/19#issuecomment-400637274
@@ -10,26 +12,23 @@ https://github.com/zalmoxisus/redux-devtools-instrument/pull/19#issuecomment-400
 reduxModule.__DO_NOT_USE__ActionTypes.REPLACE = "@@redux/INIT";
 
 // const sagaMiddleware = createSagaMiddleware();
+const bindMiddleware = (middleware) => {
+  if (process.env.NODE_ENV !== 'production') {
+    const { composeWithDevTools } = require('redux-devtools-extension')
+    return composeWithDevTools(applyMiddleware(...middleware))
+  }
 
-const composeEnhancers =
-  process.env.NODE_ENV !== "production" &&
-  typeof window === "object" &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
-    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-      })
-    : compose;
+  return applyMiddleware(...middleware)
+}
 
-const enhancer = composeEnhancers(applyMiddleware(thunk));
-
-const store = createStore(createReducer(), enhancer);
-
+const store = createStore(createReducer(), bindMiddleware([thunk, saga]));
+store.sagaTask = saga.run(rootSaga);
 store.asyncReducers = {};
 
 export const injectReducer = (key, reducer) => {
-  if (store.asyncReducers[key]) {
+  if (store.asyncReducers[key])
     return;
-  }
+
   store.asyncReducers[key] = reducer;
   store.replaceReducer(createReducer(store.asyncReducers));
   return store;
---
+++ src/app/store/rootSaga.js
@@ -0,0 +1,10 @@
+import { all, fork } from "redux-saga/effects";
+import es6promise from "es6-promise";
+
+es6promise.polyfill();
+
+export default function* rootSaga() {
+  yield all([
+    // fork(loginWatcher),
+  ]);
+}
